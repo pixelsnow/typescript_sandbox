363a803f0fcac89146f2c010af105ee6ae321c22
Vagapova Valeriia <lera.vagapova@gmail.com>
2023-01-11 10:50:06 +0200

    add fix

--- Practice110123/Practice01/js-only.js
+++ Practice110123/Practice01/js-only.js
@@ -3,7 +3,7 @@ const input1 = document.getElementById("num1");
 const input2 = document.getElementById("num2");
 
 function add(num1, num2) {
-  return num1 + num2;
+  return +num1 + +num2;
 }
 
 button.addEventListener("click", function () {
