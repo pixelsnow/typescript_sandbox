389f2c98596657b2dd73001625691b09451c0f72
Vagapova Valeriia <lera.vagapova@gmail.com>
2023-01-11 14:33:44 +0200

    add more tests

--- Practice110123/project-setup-template/app.ts
+++ Practice110123/project-setup-template/app.ts
@@ -1,3 +1,34 @@
-const person: { name: string; age: number } = { name: "James", age: 29 };
+const ADMIN = 0;
+const READ_ONLY = 1;
+const AUTHOR = 2;
+
+const person = {
+  name: "James",
+  age: 29,
+  hobbies: ["sleeping", "sports"],
+  role: "ADMIN",
+};
 
 console.log(person);
+
+const product: {
+  id: string;
+  price: number;
+  tags: string[];
+  details: {
+    title: string;
+    description: string;
+  };
+  hobbies: string[];
+} = {
+  id: "abc1",
+  price: 12.99,
+  tags: ["great-offer", "hot-and-new"],
+  details: {
+    title: "Red Carpet",
+    description: "Agreat carpet - almost brand-new!",
+  },
+  hobbies: ["swimming", "reading", "cooking"],
+};
+
+product.hobbies.forEach((hobby) => console.log(hobby.toUpperCase()));
