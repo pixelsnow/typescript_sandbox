631f65af96caed1caa09785af981dc7537586d38
Vagapova Valeriia <lera.vagapova@gmail.com>
2023-01-25 11:17:28 +0200

    add ts class

--- Practice270122/projectmanager-typescript/dist/app.js
+++ Practice270122/projectmanager-typescript/dist/app.js
@@ -1,2 +1,15 @@
 "use strict";
-console.log('PMApp in TS begins...');
+console.log("PMApp in TS begins...");
+class ProjectInput {
+    constructor() {
+        this.templateElement = document.querySelector("#project-input");
+        this.hostElement = document.querySelector("#app");
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        this.attach();
+    }
+    attach() {
+        this.hostElement.insertAdjacentElement("afterbegin", this.element);
+    }
+}
+const prjInput = new ProjectInput();
--- Practice270122/projectmanager-typescript/src/app.ts
+++ Practice270122/projectmanager-typescript/src/app.ts
@@ -1,1 +1,26 @@
-console.log('PMApp in TS begins...');
+console.log("PMApp in TS begins...");
+
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+
+  constructor() {
+    this.templateElement = document.querySelector(
+      "#project-input"
+    )! as HTMLTemplateElement;
+    this.hostElement = <HTMLDivElement>document.querySelector("#app");
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.attach();
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement("afterbegin", this.element);
+  }
+}
+
+const prjInput = new ProjectInput();
